e5483b0bf41f5c72ea4ea6cd0eb6015c23962353
hanyujin <hanyujin32432@gmail.com>
2023-05-03 14:46:34 +0900

    글작성 input file

--- post.js
+++ post.js
@@ -34,7 +34,7 @@ let button2 = maker("button", "id", "button2", div1);
 
 // 버튼1 스타일
 const div1button = document.getElementById("button1");
-div1button.style.height = "25%";
+div1button.style.height = "55%";
 div1button.style.width = "10%";
 div1button.style.marginTop = "2%";
 div1button.style.position = "absolute";
@@ -50,7 +50,7 @@ div1button.style.backgroundRepeat = "no-repeat"; //이미지 반복X
 
 // 버튼2 스타일
 const div1button2 = document.getElementById("button2");
-div1button2.style.height = "25%";
+div1button2.style.height = "55%";
 div1button2.style.width = "10%";
 div1button2.style.marginTop = "2%"; //위 마진
 div1button2.style.position = "absolute";
@@ -61,17 +61,6 @@ div1button2.style.backgroundImage = "url('./img/menu.png')";
 div1button2.style.backgroundSize = "cover";
 div1button2.style.backgroundColor = "transparent"; //버튼 색 투명
 div1button2.style.border = "none"; //테두리 X
-
-// 버튼3(모달창) 스타일
-const div1modal = document.getElementById("guide");
-div1modal.style.height = "25%";
-div1modal.style.width = "10%";
-div1modal.style.marginTop = "2%"; //위 마진
-//버튼3(모달창) 이미지
-div1modal.style.backgroundImage = "url('./img/guide.png')";
-div1modal.style.backgroundSize = "cover";
-div1modal.style.backgroundColor = "transparent"; //버튼 색 투명
-div1modal.style.border = "none"; //테두리 X
 
 // ⭐div2 스타일
 const div2 = document.getElementById("div2");
@@ -99,14 +88,56 @@ div2text.style.fontSize = "18px";
 div2text.style.textAlign = "center";
 div2text.style.margin = "0";
 
+// 버튼3(모달창) 스타일
+const div1modal = document.getElementById("guide");
+div1modal.style.height = "45%";
+div1modal.style.width = "10%";
+div1modal.style.marginTop = "4%"; //위 마진
+div1modal.style.position = "absolute";
+div1modal.style.top = "0";
+div1modal.style.right = "0";
+//버튼3(모달창) 이미지
+div1modal.style.backgroundImage = "url('./img/guide.png')";
+div1modal.style.backgroundColor = "transparent"; //버튼 색 투명
+div1modal.style.border = "none"; //테두리 X
+div1modal.style.backgroundSize = "contain"; //이미지가 다 보이게
+div1modal.style.backgroundRepeat = "no-repeat"; //이미지 반복X
+
 // ⭐div3 스타일
 const div3 = document.getElementById("div3");
-div3.style.height = "68%";
-div3.style.backgroundColor = "#325489";
-// ❗게시글 추가 if문 사용으로 게시글이 늘어나게 바꿔야함
-let div3_1 = maker("div", "id", "board", div3);
-const div3board = document.getElementById("board");
-div3board.style.backgroundColor = "#F9D67A";
-div3board.style.height = "8%";
-div3board.style.width = "100%";
-div3board.style.backgroundColor = "#537DBD";
+div3.style.height = "8%";
+div3.style.backgroundColor = "#D9D9D9";
+div3.style.borderBottom = "1px solid  black";
+
+// ⭐div4 스타일
+const div4 = document.getElementById("div4");
+div4.style.height = "55%";
+div4.style.backgroundColor = "#D9D9D9";
+div4.style.borderBottom = "1px solid  black";
+
+// ⭐div5 스타일
+const div5 = document.getElementById("div5");
+div5.style.height = "12%";
+div5.style.backgroundColor = "#D9D9D9";
+//div5안에 있는 input, 미리보기
+//fix
+let input1 = maker("input", "id", "file", div5); //모달창
+
+// input 스타일설정
+const div5input = document.getElementById("file");
+div5input.setAttribute("type", "file");
+// ⭐div6 스타일
+const div6 = document.getElementById("div6");
+div6.style.height = "10%";
+div6.style.backgroundColor = "#325489";
+
+//div6안에 있는  input submit (등록)
+let input2 = maker("input", "id", "regis", div6);
+
+const div6input = document.getElementById("regis");
+div6input.setAttribute("type", "submit");
+div6input.style.height = "100%";
+div6input.style.width = "100%";
+div6input.value = "등록";
+div6input.style.backgroundColor = "transparent"; //버튼 색 투명
+div6input.style.border = "none"; //테두리 X

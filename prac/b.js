6d0b9d48ef1b0b1595fc3720ba8d35493fa5c3d0
hanyujin <hanyujin32432@gmail.com>
2023-05-03 11:31:53 +0900

    동적 스타일링 연습

---
+++ prac/b.js
@@ -0,0 +1,13 @@
+const elementStyler = require("./a");
+
+let styleExample = {
+  height: "5%",
+  width: "25%",
+  color: "white",
+  fontWeight: "bold",
+  fontSize: "18px",
+  textAlign: "center",
+  margin: "0",
+};
+
+elementStyler("text1", styleExample, "연습");

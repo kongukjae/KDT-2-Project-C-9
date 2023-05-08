bf2fecf4d68540a6e360d34bc0fb7d0c3f03c724
hanyujin <hanyujin32432@gmail.com>
2023-05-08 10:56:29 +0900

    메인 기본 틀

---
+++ Calendar/0508/cale.js
@@ -0,0 +1,53 @@
+//body
+document.body.style.margin = "0px";
+
+// root
+let rootDiv = document.createElement("div");
+document.body.appendChild(rootDiv);
+rootDiv.setAttribute("id", "root");
+rootDiv.style.backgroundColor = "#325489";
+rootDiv.style.height = "844px";
+rootDiv.style.width = "390px";
+
+function maker(tagName, setName, setValue, parent) {
+  const element = document.createElement(tagName);
+  element.setAttribute(setName, setValue);
+  parent.appendChild(element);
+}
+// div 5개
+for (let i = 1; i < 6; i++) {
+  maker("div", "id", `div${i}`, root);
+}
+
+// ⭐div1 스타일
+const div1 = document.getElementById("div1");
+div1.style.height = "15%";
+div1.style.display = "flex";
+div1.style.justifyContent = "center";
+div1.style.flexDirection = "column";
+div1.style.position = "relative";
+
+// ⭐div2 스타일
+const div2 = document.getElementById("div2");
+div2.style.height = "20%";
+div2.style.backgroundColor = "#D9D9D9";
+div2.style.overflowY = "scroll"; //설명이 길 경우 스크롤할 수 있게 만듦
+div2.style.overflowX = "hidden"; //수평 스크롤 비활성화
+
+// ⭐div3 스타일
+const div3 = document.getElementById("div3");
+div3.style.height = "8%";
+div3.style.display = "flex";
+div3.style.alignItems = "flex-end"; //텍스트와 버튼을 하단 배치
+div3.style.justifyContent = "space-between";
+
+// ⭐div4 스타일
+const div4 = document.getElementById("div4");
+div4.style.height = "47%";
+div4.style.backgroundColor = "white";
+
+// ⭐div5 스타일
+const div5 = document.getElementById("div5");
+div5.style.height = "10%";
+div5.style.display = "flex";
+div5.style.justifyContent = "flex-end"; //버튼을 오른쪽에 배치

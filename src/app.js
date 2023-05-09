5ea0ffa907496c28fc4fc2484c9af19f7563496b
hanyujin <hanyujin32432@gmail.com>
2023-05-09 16:45:24 +0900

    ?

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+  })
+  //서버 연결
+  .listen(8080, () => {
+    console.log("8080번 서버 대기 중");
+  });

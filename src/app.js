5971713b084ad3a6c96a76aed6f291e8043c818f
hanyujin <hanyujin32432@gmail.com>
2023-05-10 17:14:53 +0900

    app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
   })
-  //서버 연결
+  //서버 연결 8080
   .listen(8080, () => {
     console.log("8080번 서버 대기 중");
   });
